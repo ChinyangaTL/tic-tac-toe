f34141aebecea6a8e1dec8c557b16c28ca02d2f2
Les Chinyanga <les.chinyanga@africacode.academy>
2023-08-16 17:05:36 +0200

    fix: remove es lint complaint

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,11 +6,12 @@ type Props = {
 };
 
 const Board: React.FC<Props> = ({ changePlayer }) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [squares, setSquares] = useState(Array(9).fill(''));
 
   return (
     <div className='board'>
-      {squares.map((square, index) => (
+      {squares.map((_, index) => (
         <Square key={index} changePlayer={changePlayer} cellIdx={index} />
       ))}
     </div>
