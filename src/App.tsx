ffd360b597a5bb3795389f78ad7246f660d6a492
Les Chinyanga <les.chinyanga@africacode.academy>
2023-08-16 16:43:05 +0200

    feat(board): render board to screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(''));
+
   return (
-    <>
-      <p>hello</p>
-    </>
+    <div className='board'>
+      {squares.map((square, index) => (
+        <button key={index} className='square'>
+          {index}
+        </button>
+      ))}
+    </div>
   );
 }
 
