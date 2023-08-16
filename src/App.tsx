f8a64d7de61f3be650c8e8276a1db145f98e2a3b
Les Chinyanga <les.chinyanga@africacode.academy>
2023-08-16 16:49:45 +0200

    feat(change-player): implement change player functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import './App.css';
 
 function App() {
   const [squares, setSquares] = useState(Array(9).fill(''));
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+
+  const changePlayer = () => {
+    currentPlayer === 'X' ? setCurrentPlayer('O') : setCurrentPlayer('X');
+  };
 
   return (
-    <div className='board'>
-      {squares.map((square, index) => (
-        <button key={index} className='square'>
-          {index}
-        </button>
-      ))}
-    </div>
+    <>
+      <p>Current player is {currentPlayer}</p>
+      <div className='board'>
+        {squares.map((square, index) => (
+          <button onClick={changePlayer} key={index} className='square'>
+            {index}
+          </button>
+        ))}
+      </div>
+    </>
   );
 }
 
